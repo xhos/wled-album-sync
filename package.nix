9cf5e71fc79d7fd9d62dd9add9780a59fe29824d
xhos <60789741+xhos@users.noreply.github.com>
2025-11-06 22:01:34 -0500

    fix: permission error

--- package.nix
+++ package.nix
@@ -15,6 +15,7 @@ stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/bin
     cp wled-album-sync.py $out/bin/wled-album-sync
+    chmod +x $out/bin/wled-album-sync
     wrapProgram $out/bin/wled-album-sync \
       --prefix PATH : ${lib.makeBinPath [uv]} \
       --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [stdenv.cc.cc.lib]}
